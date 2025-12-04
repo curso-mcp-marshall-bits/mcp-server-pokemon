bb0f587eec37343dd6a83e3833f69cab4a529bff
Marshall-Bits <kz_@hotmail.es>
2025-12-04 18:52:11 +0100

    add first tool

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import z from "zod";
 
 const server = new McpServer(
   {
@@ -14,10 +15,40 @@ const server = new McpServer(
     },
   }
 );
+
+server.registerTool(
+  "Consultar pokemon",
+  {
+    title: "Consultar los datos de un pokemon a través del nombre",
+    inputSchema: {
+      name: z.string().describe("El nombre del pokemon a buscar"),
+    },
+  },
+  async (params) => {
+    const { name } = params;
+    const pokemonData = await fetchPokemon(name);
+    return {
+      content: [{ type: "text", text: pokemonData }],
+    };
+  }
+);
+
+async function fetchPokemon(name: string) {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${name.toLocaleLowerCase().trim()}`
+  );
+  if (!response.ok) {
+    throw new Error(`Pokemon ${name} not found`);
+  }
+
+  const data = await response.json();
+
+  return `Nombre: ${data.name}, Altura: ${data.height}, Peso: ${data.weight}`;
+}
+
 async function main() {
-        const transport = new StdioServerTransport();
-        await server.connect(transport);
-    
-};
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+}
 
 main();
