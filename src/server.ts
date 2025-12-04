c01f7a29a489a23a53e398846956f0f078d1bb30
Marshall-Bits <kz_@hotmail.es>
2025-12-04 10:30:06 +0100

    add capabilities

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,23 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 
-const server = new McpServer({
-    name:"hola-mcp",
+const server = new McpServer(
+  {
+    name: "hola-mcp",
     version: "0.0.1",
-})
+  },
+  {
+    capabilities: {
+      resources: {},
+      tools: {},
+      prompts: {},
+    },
+  }
+);
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+}
+
+main();
